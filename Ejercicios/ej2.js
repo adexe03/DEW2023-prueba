c93231f0b98a4eae0cd267afc4f906974a813208
Adexe Reyes <adexe.reyes.cb@gmail.com>
2023-10-16 18:32:16 +0100

    Ejer 2,3,4 done

---
+++ Ejercicios/ej2.js
@@ -0,0 +1,18 @@
+// Media longitud de palabras
+function avgLengthWords(text) {
+     text = text.replace(/[.,:;]/g, '');
+     // const signPoints = '.,:;';
+     // signPoints.split('').forEach(s => text = text.replace(s, ''));
+     const words = text.split(" ");
+     let lengths = words.map(w => w.length);
+     return lengths.reduce((a, l) => a + l, 0) / words.length;
+}
+
+let text = ""; // 0
+console.log(text, "-> ", avgLengthWords(text));
+
+text = "Este texto tiene palabras más palabras y signos"; // 5
+console.log(text, "-> ", avgLengthWords(text));
+
+text = "Este texto tiene: palabras, más palabras y signos."; // 5
+console.log(text, "-> ", avgLengthWords(text));
---
+++ Ejercicios/ej4.js
@@ -0,0 +1,24 @@
+// Ruleta
+const REDS = [1, 3, 5, 7, 9, 12, 14, 16, 18, 21, 23, 25, 27, 30, 32, 34, 36];
+function roullete() {
+    let num = Math.floor(Math.random() * 37);
+    if (num == 0) return[0];
+    return [num, num % 2 == 0, REDS.includes(num), num >= 19];
+}
+
+console.log(roullete());
+console.log(roullete());
+console.log(roullete());
+console.log(roullete());
+console.log(roullete());
+console.log(roullete());
+console.log(roullete());
+console.log(roullete());
+console.log(roullete());
+console.log(roullete());
+console.log(roullete());
+console.log(roullete());
+console.log(roullete());
+console.log(roullete());
+console.log(roullete());
+console.log(roullete());
